b37f0152f46694c4fcd99edb5bbb456d53cc3476
Angel Huang <GoldArchex@gmail.com>
2019-02-07 14:49:28 -0800

    Add files via upload

---
+++ FRCEclipse/VisSRC 2-7-19/Robot.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include <stdlib.h>
+#include <cmath>
+//Main header files.
+#include <Robot.h>
+#include "WPILib.h"
+//Peripheral header files.
+#include "Doublesolenoid.h"
+#include "Timer.h"
+#include <AnalogGyro.h>
+#include <cmath>
+#include <ADXRS450_Gyro.h>
+#include <ctre/Phoenix.h>
+//Vision header files
+#include "CameraServer.h"
+#include "vision/VisionPipeline.h"
+#include "vision/VisionRunner.h"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include "C:\Users\GoldA\GRIP\GripPipeline.h"
+#include "C:\Users\GoldA\GRIP\GripPipeline.cpp"
+
+using namespace cs;
+using namespace std;
+
+grip::GripPipeline *visionPipeline;
+
+//frc::VisionRunner *visionThread;
+
+const int imageHeight = 480;
+const int imageWidth = 640;
+
+static void VisionThread() {
+    cs::UsbCamera camera = CameraServer::GetInstance()->StartAutomaticCapture();
+    camera.SetResolution(640, 480);
+    cs::CvSink cvSink = CameraServer::GetInstance()->GetVideo();
+    cs::CvSource outputStreamStd = CameraServer::GetInstance()->PutVideo("Gray", 640, 480);
+    cv::Mat source;
+    cv::Mat output;
+    while(true) {
+        cvSink.GrabFrame(source);
+        cvtColor(source, output, cv::COLOR_BGR2GRAY);
+        outputStreamStd.PutFrame(output);
+    }
+}
+
+void Robot::RobotInit() {
+//    UsbCamera robotVision;
+//    robotVision.SetResolution(imageWidth, imageHeight);
+//    robotVision.SetFPS(25);
+//    visionPipeline = new grip::GripPipeline();
+//
+//    cv::Mat videoSource;
+//
+//    visionThread->RunForever();
+//    visionThread->frc::VisionRunner(robotVision, visionPipeline());
+//
+//    cv::Mat videoOutput;
+
+    std::thread visionThread(VisionThread);
+    visionThread.detach();
+
+}
+
+void Robot::AutonomousInit() {
+
+}
+
+void Robot::AutonomousPeriodic() {
+
+}
+
+void Robot::TeleopInit() {
+
+}
+
+void Robot::TeleopPeriodic() {
+
+}
+
+void Robot::TestPeriodic() {
+
+}
+
+START_ROBOT_CLASS(Robot)
